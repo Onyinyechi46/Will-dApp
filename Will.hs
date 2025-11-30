52c0125c76d0f0a82a13a853dc2e556a2dde0402
Kehinde09 <odukoyakehinde09@gmail.com>
2025-11-30 03:12:25 +0100

    Enhance Will validator for partial claims
    Refactor Will validator to support partial claims and improve clarity.

--- Will.hs
+++ Will.hs
@@ -9,23 +9,24 @@
 
 module Will where
 
-import Prelude (IO, print, ($))
-import Plutus.V2.Ledger.Api (Datum(..), ScriptContext(..), TxInfo(..), PubKeyHash, Validator, POSIXTime, BuiltinData, mkValidatorScript)
-import Plutus.V2.Ledger.Contexts (txSignedBy, scriptContextTxInfo)
+import Prelude (IO, print)
+import Plutus.V2.Ledger.Api
+import Plutus.V2.Ledger.Contexts
 import Plutus.V1.Ledger.Interval (from, contains)
-import PlutusTx (compile, unstableMakeIsData)
 import qualified PlutusTx
-import PlutusTx.Prelude (Bool(..), traceIfFalse, (&&), (||), error, any, not, elem)
+import PlutusTx.Prelude
+    ( Bool(..), Integer, traceIfFalse,
+      (&&), (==), (<=), (>),
+      error, any, not )
 
 ------------------------------------------------------------
--- ON-CHAIN TYPES
+-- DATUM
 ------------------------------------------------------------
 
 data WillDatum = WillDatum
     { beneficiaries :: [PubKeyHash]
     , unlockTime    :: POSIXTime
-    , claimed       :: Bool
-    , partialClaim  :: Bool   -- New: whether partially claimed
+    , claimedAmount :: Integer
     }
 PlutusTx.unstableMakeIsData ''WillDatum
 
@@ -34,48 +35,75 @@ PlutusTx.unstableMakeIsData ''WillParam
 PlutusTx.makeLift ''WillParam
 
 ------------------------------------------------------------
--- HELPER FUNCTIONS
+-- HELPERS
 ------------------------------------------------------------
-
-{-# INLINABLE allSignedBy #-}
-allSignedBy :: TxInfo -> [PubKeyHash] -> Bool
-allSignedBy info pkhs = all (\p -> txSignedBy info p) pkhs
-  where all f [] = True
-        all f (x:xs) = f x && all f xs
 
 {-# INLINABLE anySignedBy #-}
 anySignedBy :: TxInfo -> [PubKeyHash] -> Bool
 anySignedBy info pkhs = any (txSignedBy info) pkhs
 
+{-# INLINABLE getContinuingDatum #-}
+getContinuingDatum :: ScriptContext -> WillDatum
+getContinuingDatum ctx =
+    case getContinuingOutputs ctx of
+        [o] ->
+            case txOutDatum o of
+                OutputDatum (Datum d) -> PlutusTx.unsafeFromBuiltinData d
+                _                     -> error "Expected inline datum"
+        _ -> error "Expected exactly one continuing output"
+
+{-# INLINABLE getOwnInputValue #-}
+getOwnInputValue :: ScriptContext -> Integer
+getOwnInputValue ctx =
+    let ins = txInInfoResolved <$> txInfoInputs (scriptContextTxInfo ctx)
+     in sum (map (valueOf' . txOutValue) ins)
+
+{-# INLINABLE getOwnOutputValue #-}
+getOwnOutputValue :: ScriptContext -> Integer
+getOwnOutputValue ctx =
+    case getContinuingOutputs ctx of
+        [o] -> valueOf' (txOutValue o)
+        _ -> error "Expected exactly one continuing output"
+
+{-# INLINABLE valueOf' #-}
+valueOf' :: Value -> Integer
+valueOf' v = valueOf v "" ""   -- counts ADA
+
 ------------------------------------------------------------
--- VALIDATOR LOGIC
+-- VALIDATOR
 ------------------------------------------------------------
 
 {-# INLINABLE mkWillValidator #-}
 mkWillValidator :: WillParam -> WillDatum -> () -> ScriptContext -> Bool
-mkWillValidator (WillParam ownerPkh) datum _ ctx =
+mkWillValidator (WillParam ownerPkh) oldDatum _ ctx =
     traceIfFalse "Owner signature missing" signedByOwner &&
-    traceIfFalse "No beneficiary signed" signedByBeneficiary &&
-    traceIfFalse "Already claimed" notClaimed &&
-    traceIfFalse "Too early to unlock funds" deadlineReached &&
-    traceIfFalse "Partial claim already done" notPartialClaim
+    traceIfFalse "Beneficiary signature missing" signedByBeneficiary &&
+    traceIfFalse "Too early" timeOK &&
+    traceIfFalse "Withdrawal amount invalid" validWithdraw &&
+    traceIfFalse "Claim amount mismatch" correctDatumUpdate &&
+    traceIfFalse "Overclaiming" notOverclaim
   where
     info = scriptContextTxInfo ctx
 
-    signedByOwner :: Bool
-    signedByOwner = txSignedBy info ownerPkh
+    signedByOwner        = txSignedBy info ownerPkh
+    signedByBeneficiary  = anySignedBy info (beneficiaries oldDatum)
 
-    signedByBeneficiary :: Bool
-    signedByBeneficiary = anySignedBy info (beneficiaries datum)
+    timeOK = contains (from $ unlockTime oldDatum) (txInfoValidRange info)
 
-    notClaimed :: Bool
-    notClaimed = not (claimed datum)
+    newDatum = getContinuingDatum ctx
 
-    notPartialClaim :: Bool
-    notPartialClaim = not (partialClaim datum)
+    inputValue  = getOwnInputValue ctx
+    outputValue = getOwnOutputValue ctx
 
-    deadlineReached :: Bool
-    deadlineReached = contains (from $ unlockTime datum) (txInfoValidRange info)
+    withdrawAmount = inputValue - outputValue
+
+    validWithdraw = withdrawAmount > 0
+
+    correctDatumUpdate =
+        claimedAmount newDatum == claimedAmount oldDatum + withdrawAmount
+
+    notOverclaim =
+        claimedAmount newDatum <= inputValue     -- cannot inflate
 
 ------------------------------------------------------------
 -- WRAPPER
@@ -83,27 +111,19 @@ mkWillValidator (WillParam ownerPkh) datum _ ctx =
 
 {-# INLINABLE wrappedValidator #-}
 wrappedValidator :: WillParam -> BuiltinData -> BuiltinData -> BuiltinData -> ()
-wrappedValidator p datum _ ctx =
-    check $ mkWillValidator p d () c
+wrappedValidator p d _ c =
+    check $ mkWillValidator p datum () ctx
   where
-    d = PlutusTx.unsafeFromBuiltinData datum
-    c = PlutusTx.unsafeFromBuiltinData ctx
+    datum = PlutusTx.unsafeFromBuiltinData d
+    ctx   = PlutusTx.unsafeFromBuiltinData c
     check True  = ()
     check False = error ()
-
-------------------------------------------------------------
--- COMPILED PARAMETERISED VALIDATOR GENERATOR
-------------------------------------------------------------
 
 validatorCode :: PlutusTx.CompiledCode (WillParam -> BuiltinData -> BuiltinData -> BuiltinData -> ())
 validatorCode = $$(PlutusTx.compile [|| wrappedValidator ||])
 
 mkWillInstance :: WillParam -> Validator
-mkWillInstance param = mkValidatorScript (validatorCode `PlutusTx.applyCode` PlutusTx.liftCode param)
-
-------------------------------------------------------------
--- MAIN
-------------------------------------------------------------
+mkWillInstance p = mkValidatorScript (validatorCode `PlutusTx.applyCode` PlutusTx.liftCode p)
 
 main :: IO ()
-main = print "✅ Extended Parameterized Will validator compiled successfully!"
+main = print "✅ Partial-claim parameterized will compiled!"
